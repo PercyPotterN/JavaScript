67c22835119737acca421189d586f95eb19e06a0
PercyPotterN <111822433+PercyPotterN@users.noreply.github.com>
2023-01-08 11:55:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name ="Sonic";
+console.log(first_name);
+
+var last_name =" The Hedgehog";
+console.log(last_name);
+
+var full_name =first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_name = full_name.length;
+console.log(length_of_name);
